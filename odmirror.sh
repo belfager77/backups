ca09a83dbee14fd04f5e2397f95fc209275fe8b1
simon <si_rutter@hotmail.com>
2026-04-22 18:43:00 +0800

    Added logging to OD mirror

--- odmirror.sh
+++ odmirror.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rclone sync /home/simon/backup onedrive:backups --progress
+rm /home/simon/src/backups/odmirror.log
+rclone sync /home/simon/backup onedrive:backups --log-file=/home/simon/src/backups/odmirror.log
